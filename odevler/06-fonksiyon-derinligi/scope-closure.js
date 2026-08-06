e8f757f0d261e3473862786ec89516b026b16bc2
byzakeskin <byzanurkeskin001@gmail.com>
2026-08-06 12:42:19 +0300

    feat(odevler): add closure and reduce exercises

--- odevler/06-fonksiyon-derinligi/scope-closure.js
+++ odevler/06-fonksiyon-derinligi/scope-closure.js
@@ -164,39 +164,134 @@ console.log(a);
 //bu sefer b = 20 shadowinge uğrayacak
 
 //fonksiyon bildirimi
-console.log(topla(11,2))
-function topla(a,b){
-    return a+b
+console.log(topla(11, 2));
+function topla(a, b) {
+  return a + b;
 }
-console.log(topla(11,2))
+console.log(topla(11, 2));
 
 //fonksiyon ifadesi
 //console.log(topla2(11,2)) ->ReferenceError
-const topla2 = function(a,b){return a+b}
-console.log(topla2(11,2))
+const topla2 = function (a, b) {
+  return a + b;
+};
+console.log(topla2(11, 2));
 
 //arrow function
 //console.log(topla3(11,2)) ->ReferenceError
-const topla3 = (a,b) => {return a+b}
-console.log(topla3(11,2))
+const topla3 = (a, b) => {
+  return a + b;
+};
+console.log(topla3(11, 2));
 
 //CLOSURES
 
 function sayacUret() {
-    let sayac = 0;
+  let sayac = 0;
 
-    return function () {
-        sayac+=1;
-        return sayac;
-    };
+  return function () {
+    sayac += 1;
+    return sayac;
+  };
 }
 
 const sayac1 = sayacUret();
 const sayac2 = sayacUret();
 
-console.log(sayac1()); 
-console.log(sayac1()); 
-console.log(sayac1()); 
+console.log(sayac1());
+console.log(sayac1());
+console.log(sayac1());
 
-console.log(sayac2()); 
+console.log(sayac2());
+
+function hesapAc(baslangicBakiyesi) {
+  let bakiye = baslangicBakiyesi;
+
+  function yatir(para) {
+    bakiye += para;
+    console.log(`${para}₺ yatırıldı. Bakiyeniz: ${bakiye}₺`);
+  }
+
+  function cek(para) {
+    if (para > bakiye) {
+      console.log(
+        `Paran yok? 🤨 Neden ${bakiye}₺ olan hesaptan, ${para}₺ çekmeye çalışıyosun?`,
+      );
+      return;
+    }
+    bakiye -= para;
+    console.log(`${para}₺ çekildi. Bakiyeniz: ${bakiye}₺`);
+  }
+
+  function bakiyeGoster() {
+    console.log(`Bakiyeniz: ${bakiye}₺`);
+    return bakiye;
+  }
+
+  return { yatir, cek, bakiyeGoster };
+}
+
+const hesap = hesapAc(10);
+
+hesap.bakiyeGoster();
+hesap.yatir(5);
+hesap.cek(20);
+hesap.cek(10);
+
+console.log(hesap.bakiye);
+hesap.bakiye = 100;
+console.log(`Gerçek paran bu mu? ${hesap.bakiye}₺ mi?`);
+
+hesap.bakiyeGoster();
+
+function myForEach(dizi, callback) {
+  for (let i = 0; i < dizi.length; i++) {
+    callback(dizi[i], i, dizi);
+  }
+}
+
+function myMap(dizi, callback) {
+  const sonuc = [];
+  for (let i = 0; i < dizi.length; i++) {
+    sonuc.push(callback(dizi[i], i, dizi));
+  }
+  return sonuc;
+}
+
+const rakamlar = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log("forEach:");
+rakamlar.forEach((s) => console.log(s+1));
+
+console.log("myForEach:");
+myForEach(rakamlar, (s) => console.log(s+1));
+
+
+console.log("map:", rakamlar.map((s) => s+1));
+console.log("myMap:", myMap(rakamlar, (s) => s+1));
+
+console.log("Orijinal dizi hâlâ aynı mı:", rakamlar);
+
+//REDUCE
+let gorevler = [
+    { id: 1, metin: "ekmek al", bitti: false },
+    { id: 2, metin: "faturayi ode", bitti: true }
+];
+// Tahminim, gorevler içinde bitti:true olan 1 eleman var,
+// bu yüzden reduce sonucu 1 olmalı
+const bitenGorev = gorevler.reduce((deger, gorev) => {
+    if (gorev.bitti) {
+        return deger + 1; 
+    }
+    return deger; 
+}, 0); 
+
+console.log("reduce ile biten gorev sayısı:", bitenGorev);
+
+
+// Tahminim, filter().length ile de 1 çıktısı alırım
+// çünkü filter bitti:true olanları eleyip yeni bir dizi döndürür,
+// length de o dizinin eleman sayısını verir
+const bitenGorevFilter = gorevler.filter((gorev) => gorev.bitti).length;
+
+console.log("filter().length ile bitmiş gorev sayısı:", bitenGorevFilter);
 
